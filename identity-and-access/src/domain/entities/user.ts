63e582b8d17de9acada937615edd62c2ae453f72
Vincent <jouanne.vincent@gmail.com>
2021-08-17 17:01:42 +0200

    refactor: define explicitly userid

--- identity-and-access/src/domain/entities/user.ts
+++ identity-and-access/src/domain/entities/user.ts
@@ -4,10 +4,11 @@ import { UUID } from '@identity-and-access/domain/value-objects/uuid';
 import { HashedPassword } from '../value-objects/password';
 import { EmailInfos } from '../value-objects/emailInfos';
 
+export const UserId = UUID.withBrand('UserId');
+
 export const User = Record({
-  id: UUID.withBrand('UserId'),
+  id: UserId,
   email: EmailInfos,
   password: HashedPassword,
 });
-
 export type User = Static<typeof User>;

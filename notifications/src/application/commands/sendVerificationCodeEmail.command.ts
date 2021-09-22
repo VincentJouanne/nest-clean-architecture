169c5e86e4cfd97f49761442ff2f20c95219a102
VincentJouanne <jouanne.vincent@gmail.com>
2021-09-22 13:49:18 +0200

    refactor(notifications): rename sendEmail use-case and add verification code in payload

--- notifications/src/application/commands/sendVerificationCodeEmail.command.ts
+++ notifications/src/application/commands/sendVerificationCodeEmail.command.ts
@@ -1,13 +1,15 @@
 import { PinoLoggerService } from "@common/logger/adapters/real/pinoLogger.service";
 import { executeTask } from "@common/utils/executeTask";
+import { fromUnknown } from "@common/utils/fromUnknown";
 import { noop } from "@common/utils/noop";
 import { perform } from "@common/utils/perform";
 import { VerificationCode4 } from "@identity-and-access/domain/value-objects/verificationCode4";
 import { CommandHandler, ICommand, ICommandHandler } from "@nestjs/cqrs";
 import { Email } from "@notifications/domain/value-objects/email";
+import { VerificationCodeEmailPayload } from "@notifications/domain/value-objects/verificationCodeEmailPayload";
 import { InMemoryMailService } from "@notifications/infrastructure/adapters/secondaries/fake/inMemoryMail.service";
 import { pipe } from "fp-ts/lib/function";
-import { map } from "fp-ts/lib/TaskEither";
+import { chain, map } from "fp-ts/lib/TaskEither";
 
 export class SendVerificationCodeEmail implements ICommand {
     constructor(
@@ -21,6 +23,10 @@ export class SendVerificationCodeEmailHandler implements ICommandHandler {
     }
     execute(command: SendVerificationCodeEmail): Promise<void> {
         const { email, verificationCode } = command;
-        return executeTask(pipe(perform(email, this.mailService.sendEmail, this.logger, 'send verification code email'), map(noop)));
+        return executeTask(
+            pipe(
+            fromUnknown({to: email, verificationCode: verificationCode}, VerificationCodeEmailPayload, this.logger, 'verification code email payload'),
+            chain((payload) => perform(payload, this.mailService.sendEmail, this.logger, 'send verification code email')), 
+            map(noop)));
     }
 }

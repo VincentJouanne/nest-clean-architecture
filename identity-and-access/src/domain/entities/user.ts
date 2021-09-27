33cd7934cf3e081b6adf1aafe3400bc235219ee5
VincentJouanne <jouanne.vincent@gmail.com>
2021-09-27 15:58:30 +0200

    refactor(user): add verifyEmail domain action

--- identity-and-access/src/domain/entities/user.ts
+++ identity-and-access/src/domain/entities/user.ts
@@ -1,7 +1,10 @@
+import { fromUnknown } from '@common/utils/fromUnknown';
 import { UUID } from '@identity-and-access/domain/value-objects/uuid';
+import { LoggerService } from '@nestjs/common';
+import { TaskEither } from 'fp-ts/lib/TaskEither';
 import { Record, Static } from 'runtypes';
-import { HashedPassword } from '../value-objects/password';
 import { ContactInformation } from '../value-objects/contactInformation';
+import { HashedPassword } from '../value-objects/password';
 
 export const UserId = UUID.withBrand('UserId');
 export type UserId = Static<typeof UserId>;
@@ -12,3 +15,8 @@ export const User = Record({
   contactInformation: ContactInformation,
 });
 export type User = Static<typeof User>;
+
+
+export const verifyUserEmail = (user: User, logger: LoggerService): TaskEither<Error, User> => {
+  return fromUnknown({ ...user, contactInformation: { ...user.contactInformation, isVerified: true } }, User, logger, 'user')
+}
